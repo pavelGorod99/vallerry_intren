3663a9c9e5d691a239c785933b6fba39ef3ca4da
pavelGorod99 <pavel.gorodetchi@gmail.com>
2022-04-05 17:26:53 +0200

    Updated Section 2 and Added Section 3

--- Section_2/src/exercises/Test.java
+++ Section_2/src/exercises/Test.java
@@ -1,8 +1,8 @@
-package exercises;
+package Section_2.src.exercises;
 
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world from Section 2");
     }
 }
