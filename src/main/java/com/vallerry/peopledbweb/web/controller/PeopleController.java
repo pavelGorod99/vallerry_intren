d3985572bf305dce134fe74a80ab468b78a7f995
pavelGorod99 <pavel.gorodetchi@gmail.com>
2022-05-12 16:08:26 +0200

    Validating Form

--- src/main/java/com/vallerry/peopledbweb/web/controller/PeopleController.java
+++ src/main/java/com/vallerry/peopledbweb/web/controller/PeopleController.java
@@ -4,10 +4,13 @@ import com.vallerry.peopledbweb.biz.model.Person;
 import com.vallerry.peopledbweb.data.PersonRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -35,9 +38,11 @@ public class PeopleController {
     }
 
     @PostMapping
-    public String savePerson(Person person) {
-        System.out.println(person.toString());
-        personRepository.save(person);
-        return "redirect:people";
+    public String savePerson(@Valid Person person, Errors errors) {
+        if (!errors.hasErrors()) {
+            personRepository.save(person);
+            return "redirect:people";
+        }
+        return "people";
     }
 }
